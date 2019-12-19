b8defa7245ae387bc12cefcdcbb47bd1aa7fdf57
pamccombs <34119196+pamccombs@users.noreply.github.com>
2019-12-19 15:05:12 -0500

    begin handleOnClick for APIValuesCard

--- src/components/APIValueCard.js
+++ src/components/APIValueCard.js
@@ -1,16 +1,25 @@
 import React from 'react'
 
 const APIValueCard = (api_value) => {
-    console.log(api_value.api_value)
-    return(
-    <div key={api_value.id} className="APIValuesCard">
-        <h3>{api_value.api_value.pct_slider}</h3>
-        <h3>{api_value.api_value.comp_person_name}</h3>
-        <h3>{api_value.api_value.comp_person_email}</h3>
-        <h3>{api_value.api_value.bench_salary}</h3>
-        <h3>{api_value.api_value.offer_salary}</h3>
-        <br></br>
-    </div>
+    
+    const handleOnClick = e => {
+        e.preventDefault()
+        console.log(api_value.api_value.id)
+    }
+
+    return (
+        <div
+            key={api_value.api_value.id}
+            className="APIValuesCard"
+            onClick={handleOnClick}
+        >
+            <h3>{api_value.api_value.pct_slider}</h3>
+            <h3>{api_value.api_value.comp_person_name}</h3>
+            <h3>{api_value.api_value.comp_person_email}</h3>
+            <h3>{api_value.api_value.bench_salary}</h3>
+            <h3>{api_value.api_value.offer_salary}</h3>
+            <br></br>
+        </div>
     )
 }
 
